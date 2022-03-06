c983a0bd2f303a3ecb28a140149ec007a0474c63
Tanmay Fadnavis <fadnavistanmay@gmail.com>
2022-03-06 14:35:56 -0800

    Prototype and singleton

---
+++ DesignPatterns/Prototype/Prototype.cs
@@ -0,0 +1,55 @@
+﻿public abstract class Prototype
+{
+    string name;
+
+    public Prototype(string name)
+    {
+        this.name = name;
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public abstract Prototype Clone();
+}
+
+public class ConcretePrototype1: Prototype
+{
+    public ConcretePrototype1(string name): base(name)
+    {
+
+    }
+
+    //Creates a shallow copy.
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+}
+
+public class ConcretePrototype2 : Prototype
+{
+    public ConcretePrototype2(string name) : base(name)
+    {
+
+    }
+
+    //Creates a shallow copy.
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+}
+
+public class Client
+{
+    public static void Main()
+    {
+        ConcretePrototype1 p1 = new ConcretePrototype1("Prototyp_one");
+        ConcretePrototype1 c1 = (ConcretePrototype1)p1.Clone();
+        Console.WriteLine($"Cloned name is {c1.Name}");
+
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿public class Singleton
+{
+    static Singleton singleton;
+
+    protected Singleton()
+    {
+    }
+
+    public static Singleton Instance()
+    {
+        if (singleton == null)
+        {
+            singleton = new Singleton();
+        }
+
+        return singleton;
+    }
+}
+
+public class Client
+{
+    public static void Main()
+    {
+        Singleton singleton1 = Singleton.Instance();
+        Singleton singleton2 = Singleton.Instance();
+
+        if (singleton1 == singleton2)
+        {
+            Console.WriteLine("Same instance!!");
+        }
+    }
+}
